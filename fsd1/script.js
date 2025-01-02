95271ed6090c0b3f5c208b77ba274c8dfc323bab
MOHAMMED ABUBAKAR SIDDIQ <143837684+Abu999b@users.noreply.github.com>
2025-01-02 11:00:00 +0530

    Add files via upload

---
+++ fsd1/script.js
@@ -0,0 +1,8 @@
+// script.js
+// Add interactivity or animations here
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta');
+    ctaButton.addEventListener('click', () => {
+      alert('Let the games begin!');
+    });
+  });
